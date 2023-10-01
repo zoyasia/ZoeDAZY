f5556e7c580d4bf3e0de681baef015b85db7c17c
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-10-01 21:12:10 +0200

    make:auth OK

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LoginController extends AbstractController
+class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_login')]
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
         ]);
     }
 }
