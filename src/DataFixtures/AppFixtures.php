412a98c4426901f36cf663e31090d0a378fb6332
Zoé_Dazy <zoe.dazy+github@gmail.com~>
2023-10-01 20:57:06 +0200

    ajout fixtures à la mano

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,40 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $hasher)
+    {
+    }
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $regularUser = new User();
+        $regularUser
+        ->setEmail('bobby@bob.com')
+        ->setPassword($this->hasher->hashPassword($regularUser, 'test'))
+        ->setFirstname('Bobby')
+        ->setLastname('Brown')
+        ->setPicture('https://via.placeholder.com/640x480.png/00ee33?text=rerum')
+        ->setRoles(['ROLE_USER']);
+        
+        $manager->persist($regularUser);
+
+        $adminUser = new User();
+        $adminUser
+        ->setEmail('admin@mycorp.com')
+        ->setRoles(['ROLE_ADMIN'])
+        ->setPassword($this->hasher->hashPassword($adminUser, 'test'))
+        ->setFirstname('Marie')
+        ->setLastname('Curie')
+        ->setPicture('https://via.placeholder.com/640x480.png/00ee33?text=rerum');
+
+  $manager->persist($adminUser);
+
 
         $manager->flush();
     }
